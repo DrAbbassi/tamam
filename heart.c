eec04377223f03ac6c4c28627fba4357fa73bf88
DrAbbassi <103210974+DrAbbassi@users.noreply.github.com>
2023-02-11 21:29:39 +0000

    heh

---
+++ heart.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+
+void heart() {
+    double x, y, z, f;
+
+    for (y=1.5;y>-1.5;y-=0.095) 
+    {    
+        for (x=-1.5;x<1.5;x+=0.05)
+        {
+            z = x*x+y*y-1;
+            f = z*z*z-x*x*y*y*y;
+
+            if (f <= 0) printf("%C", 2);
+            else printf(" ");
+
+            putchar('\n');
+
+        }
+    }
+}
+
+int main() {
+
+    printf("Tamam\n");
+    heart(); 
+}
